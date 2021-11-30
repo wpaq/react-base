f92aac62ea0d27824960eb0c56f4602af86606f2
wpaq <wallysonpabloo@gmail.com>
2021-11-29 21:44:47 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:prettier/recommended',
+    ],
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 13,
+        sourceType: 'module',
+        requireConfigFile: false,
+        babelOptions: {
+            presets: ['@babel/preset-react'],
+        },
+    },
+    plugins: ['react', 'prettier', 'react-hooks'],
+    rules: {
+        'prettier/prettier': 0,
+        'react/jsx-filename-extension': 0,
+        'import/prefer-default-export': 0,
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'react/jsx-runtime': 0,
+    },
+}
