66a450618ed605c151379e02e0d7f69f0d52cbef
wpaq <wallysonpabloo@gmail.com>
2021-12-05 10:51:39 -0300

    prop-types erro corrigido

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,5 +29,6 @@ module.exports = {
         'react-hooks/exhaustive-deps': 'warn',
         'react/jsx-runtime': 0,
         'no-unused-vars': 0,
+        "react/jsx-props-no-spreading": ["error", {"custom": "ignore"}]
     },
 }
